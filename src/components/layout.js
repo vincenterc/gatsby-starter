b4caf467acb87159b4745df81548b4575c5a7e9b
vincenterr <chung.menghsun@gmail.com>
2019-03-05 20:49:06 +0800

    Add title, description meta and favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+
+import favicon from '../../assets/img/favicon.ico'
 import './layout.css'
 
 class Layout extends React.Component {
   render() {
     let { children } = this.props
 
-    return <Wrapper>{children}</Wrapper>
+    return (
+      <Wrapper>
+        <Helmet>
+          <title>Gatsby Starter</title>
+          <meta name="description" content="Gatsby starter..." />
+          <link rel="icon" href={favicon} />
+        </Helmet>
+        {children}
+      </Wrapper>
+    )
   }
 }
 
