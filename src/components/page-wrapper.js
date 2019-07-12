2c0ff0a009f02cc9bc43e922dc12005fb5953bed
vincenterr <chung.menghsun@gmail.com>
2019-07-12 18:01:34 +0800

    Hook redux in gatsby-ssr and gatsby-browser api

--- src/components/page-wrapper.js
+++ src/components/page-wrapper.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import store from '../store'
 import Layout from './layout'
 
 const PageWrapper = Comp => {
   class Wrapper extends React.Component {
     render() {
       return (
-        <Provider store={store}>
-          <Layout {...this.props}>
-            <Comp {...this.props} />
-          </Layout>
-        </Provider>
+        <Layout {...this.props}>
+          <Comp {...this.props} />
+        </Layout>
       )
     }
   }
